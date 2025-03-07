3e9cd4309f9630d6503855a58b039e223a7c949d
taco323 <guillermobautista721@gmail.com>
2025-03-06 22:00:04 -0600

    act

--- Tarea 2/script.js
+++ Tarea 2/script.js
@@ -1,6 +1,9 @@
-let p1= "escribe aqui w";
-let p2= "párrafo 2";
+let nodo1 = "Pienso que la tecnología web es fundamental en mi formación en artes y comunicación porque me permite acercarme a los lenguajes y herramientas que definen la creación de contenido en el mundo digital. Aprender esto me ayuda a comprender la estructura de la información en la web, lo que es esencial para organizar mis ideas y mensajes de manera coherente. Me ayuda a introducirme al mundo del diseño visual, permitiéndome entender cómo se construye la estética y la identidad de un proyecto digital, así cómo a la lógica de la interactividad, un aspecto clave en la comunicación moderna, donde la participación del usuario es algo clave. Estas tecnologías me permiten entender cómo se construye y se consume el contenido en la epoca contemporanea. "
 
-document.getElementById('parr1').innerHTML=p1;
+let nodo2 = "Además, el estudio de estas herramientas me da una perspectiva más amplia sobre cómo se integran el arte, la comunicación y la tecnología. Considero que no se trata solo de aprender a usarlas, sino de comprender su impacto en la forma en que nos relacionamos con el contenido digital. Esto me ayuda a desarrollar un pensamiento más crítico sobre cómo se diseñan las experiencias digitales y cómo influyen en la percepción del público. En un mundo donde lo digital está en todos lados, entender estas tecnologías me permite estar mejor preparado para los desafíos y oportunidades que surgen en mi campo profesional."
 
-document.getElementById('parr2').innerHTML=p2;
+document.getElementById('nodo1').innerHTML=nodo1;
+document.getElementById('nodo2').innerHTML=nodo2;
+
+console.log(nodo1.length);
+console.log(nodo2.length);

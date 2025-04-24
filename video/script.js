ab701f527146d60899c4fa231b004a84d0dac359
taco323 <guillermobautista721@gmail.com>
2025-04-23 22:55:20 -0600

    actola

---
+++ video/script.js
@@ -0,0 +1,7 @@
+console.log("La influencia del manga en mi vida");
+
+// Definimos el contenido que queremos mostrar en el párrafo
+let p1 = "Fortnite ha marcado una etapa importante en mi vida, transformando mi enfoque hacia los videojuegos y mi forma de enfrentar retos.";
+
+// Asignamos el contenido a un elemento HTML con id 'parr1'
+document.getElementById('parr1').innerHTML = p1;
